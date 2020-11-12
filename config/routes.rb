dc91ab4970a66d7308a0c27eb307356de7aade11
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-12 13:27:26 -0500

    routes done and controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  get "/cocktails", to: "cocktails#index"
+  get "/cocktails/new", to: "cocktails#new"
+  post "/cocktails", to: "cocktails#create" 
+  get "/cocktails/:id", to: "cocktails#show"
+  get "/cocktails/:cocktail_id/doses/new", to: "doses#new"
+  post "/cocktails/:cocktail_id/doses/", to: "doses#create"
+  delete "/doses/:id", to: "doses#destroy"
 end
