a15e87eb1d3fbda72d5c175fb6e84d34bab04b19
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-12 14:22:33 -0500

    changement dans les routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  get "/cocktails", to: "cocktails#index"
-  get "/cocktails/new", to: "cocktails#new"
-  post "/cocktails", to: "cocktails#create" 
-  get "/cocktails/:id", to: "cocktails#show"
-  get "/cocktails/:cocktail_id/doses/new", to: "doses#new"
-  post "/cocktails/:cocktail_id/doses/", to: "doses#create"
-  delete "/doses/:id", to: "doses#destroy"
+  # get "/cocktails", to: "cocktails#index"
+  # get "/cocktails/new", to: "cocktails#new", as: "new_cocktail"
+  # post "/cocktails", to: "cocktails#create" 
+  # get "/cocktails/:id", to: "cocktails#show", as: "cocktail"
+  # get "/cocktails/:cocktail_id/doses/new", to: "doses#new" as: "new_cocktail_dose"
+  # post "/cocktails/:cocktail_id/doses/", to: "doses#create" as: "cocktail_doses"
+  # delete "/doses/:id", to: "doses#destroy"
+  resources :cocktails, only:[:index, :new, :create, :show] do
+   resources :doses, only:[:new, :create]
+  end
 end
