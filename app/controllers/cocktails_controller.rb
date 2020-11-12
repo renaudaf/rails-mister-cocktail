7490bcce2bca20a2323eaf24ec588818c6954811
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-12 16:02:36 -0500

    rake 100%

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -16,9 +16,11 @@ class CocktailsController < ApplicationController
 
  def create
   @cocktail = Cocktail.new(cocktail_params)
-  @cocktail.save
-
-  redirect_to cocktails_path
+  if @cocktail.save
+   redirect_to cocktail_path(@cocktail)
+  else
+   render :new
+  end
  end
 
  def destroy
